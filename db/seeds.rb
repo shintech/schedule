d02e8c46bec81d625a9820552752b48e7b99e62f
Michael Prather <mprather1@gmail.com>
2016-05-30 13:59:38 -0500

    displaying pay periods

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 day = 0
 work_dayid = 1
+
 5.times do |x|
   Punch.create(in: DateTime.now + day.hours, out: DateTime.now + day.hours + 8.hours, work_day_id: work_dayid, user_id: 1)
   day += 24
   work_dayid += 1
 end
-day = 0
-5.times do |x|
 
-  WorkDay.create(user_id: 1, work_date: DateTime.now + day.hours)
+day = 0
+
+5.times do |x|
+  WorkDay.create(user_id: 1, work_date: DateTime.now + day.hours, pay_period_id: 1)
   day += 24
 end
+
+PayPeriod.create
+
 User.create(first_name: "Nick", last_name: "Cage")
